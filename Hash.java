a207a18d902e643794cd2f80f90977da5e92f71e
Али Абдуллаев <abdullaevali20007@gmail.com>
2025-10-31 23:20:40 +0300

    Create Hash.java with HashMap example

---
+++ Hash.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+public class hash {
+    public static void main(String[] args) {
+        HashMap<String, Integer> m = new HashMap<>();
+        m.put("apple", 3);
+        System.out.println(m.get("apple"));
+    }
+}
